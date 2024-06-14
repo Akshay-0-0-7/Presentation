b156a8d474fbdec64b00465d641ce8f5f30b4250
Akshay Uppu <uppuakshay07@gmail.com>
2024-06-14 19:53:25 +0530

    Changes made to code

---
+++ OutKeyword/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OutKeyword
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string fullName = "John Doe";
+            string firstName, lastName;
+
+            // Call the method to split the full name into first and last names
+            SplitName(fullName, out firstName, out lastName);
+
+            // Print the results
+            Console.WriteLine($"First Name: {firstName}");
+            Console.WriteLine($"Last Name: {lastName}");
+        }
+
+        static void SplitName(string fullName, out string firstName, out string lastName)
+        {
+            // Split the full name by space
+            string[] nameParts = fullName.Split(' ');
+
+            // Assign the split parts to the out parameters
+            firstName = nameParts[0];
+            lastName = nameParts[1];
+        }
+    }
+}
---
+++ RefKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RefKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            Console.WriteLine($"number before modifying: {number}");
+
+            ModifyInteger(ref number);
+
+            Console.WriteLine($"number after modifying: {number}");
+
+        }
+
+        static void ModifyInteger(ref int value)
+        {
+            value += 10;
+
+            //Console.WriteLine($"Modifying without ref keyword: {value}");
+        }
+    }
+}
